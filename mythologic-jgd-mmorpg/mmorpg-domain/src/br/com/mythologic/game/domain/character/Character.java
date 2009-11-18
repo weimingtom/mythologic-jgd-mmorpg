c9edb14b8f138151c70d9a32697fddb35301004d
valezin <valezin@0404d4ee-d3ee-11de-901b-a378632c6355>
2009-11-18 12:48:22 +0000

    interface Thing has getName() already

--- mythologic-jgd-mmorpg/mmorpg-domain/src/br/com/mythologic/game/domain/character/Character.java
+++ mythologic-jgd-mmorpg/mmorpg-domain/src/br/com/mythologic/game/domain/character/Character.java
@@ -4,8 +4,5 @@ import br.com.mythologic.game.domain.character.attributes.Gender;
 import br.com.mythologic.game.domain.thing.Thing;
 
 public interface Character extends Thing {
-	public String getName();
-	public Gender getGender();	
-	
-	
+	public Gender getGender();
 }
